1c2edccb2b584f4c7caf86f79cdcd9fa3aae0df4
JefferyWCG <cw3736@nyu.edu>
2023-05-14 21:39:54 -0400

    add README.md for requests

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,14 @@
 const path = require('path')
 
-
+console.log(process.env.NODE_ENV)
 //the state of server, export as a global object
 const JEAT = {
-    logger:require("./core/logger").init('MASTER'),
+    IS_DEBUG: process.env.NODE_ENV == 'development', //configuration mode
+    logger:require("./core/logger").init('JEAT'),
     kernel:require("./core/kernel"),
-    config:require(".core/config")
+    config:require("./core/config")
 }
-
 global.JEAT = JEAT
+
+JEAT.logger.info(`The server will be running in ${JEAT.IS_DEBUG?"development":"master"} mode`)
+
